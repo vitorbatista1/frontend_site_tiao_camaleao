1296e6a103b04d3fc2ba73bfa61b4c81535251b0
Vitor Batista Pessoal <vitorbatista177@outlook.com>
2026-05-07 21:38:33 -0300

    fix: corrigindo problemas de build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
 
 export default defineConfig({
-  output: 'hybrid',
-  adapter: node({ mode: 'standalone' }),
+  output: 'static',
   integrations: [
     react(),
     tailwind({ applyBaseStyles: false }),
